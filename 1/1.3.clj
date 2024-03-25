90867a93021984b84457b82a3d916f1c2b1eb247
Poiku <william@sonesson.org>
2024-03-25 16:49:53 +0100

    gjorde så att man deklarerade mängden direkt esketit

--- 1/1.3.clj
+++ 1/1.3.clj
@@ -1,21 +1,17 @@
 ; ; 1.3
 (def A #{3 4 5 6 7 8})
 
-(defn calculate-b [set-A]
-  (let [pairs (for [a set-A
-                    b set-A
-                    :when (not= a + b 0)]
+(def B
+  (let [pairs (for [a A
+                    b A :when (not= a + b 0)]
                 [a b])]
     (set (map (fn [[a b]] (/ (- a b) (+ a b))) pairs))))
+    
+(println "The length of B is:" (count B))
 
-(def B (calculate-b A))
-(println "The length of B is: " (count B))
-
-(defn calculate-c [set-A]
-  (let [pairs (for [a set-A
-                    b set-A
-                    :when (and (not= b 0) (<= a b))] [a b])]
+(def C
+  (let [pairs (for [a A
+                    b A :when (and (not= b 0) (<= a b))] [a b])]
     (set (map (fn [[a b]] (/ a b)) pairs))))
 
-(def C (calculate-c A))
-(println "The length of C is: " (count C))
+(println "The length of C is:" (count C))
--- 1/1.4.clj
+++ 1/1.4.clj
@@ -1,29 +1,27 @@
 ; ; 1.4
 (def A #{1 2 3 4 5 6})
-(defn calculate-b [set-A]
-  (let [pairs (for [a set-A
-                    b set-A
-                    ][a b])]
+
+(def B
+  (let [pairs (for [a A
+                    b A]
+                    [a b])]
     (set (map (fn [[a b]] (/ a b)) pairs))))
 
-(def B (calculate-b A))
-(println "The length of B is: " (count B))
+(println "The length of B is:" (count B))
 
-(defn calculate-c [set-A]
-  (let [pairs (for [a set-A
-                    b set-A :when (= (rem a b) 0)]
+(def C
+  (let [pairs (for [a A
+                    b A :when (= (rem a b) 0)]
                 [a b])]
     (set (map (fn [[a b]] (/ a b)) pairs))))
 
-(def C (calculate-c A))
-(println "The length of C is: " (count C))
+(println "The length of C is:" (count C))
 
 
-(defn calculate-d [set-A]
-  (let [pairs (for [a set-A
-                    b set-A :when (= (rem b a) 0)]
+(def D
+  (let [pairs (for [a A
+                    b A :when (= (rem b a) 0)]
                 [a b])]
     (set (map (fn [[a b]] (/ a b)) pairs))))
 
-(def D (calculate-d A))
-(println "The length of D is: " (count D))
+(println "The length of D is:" (count D))
